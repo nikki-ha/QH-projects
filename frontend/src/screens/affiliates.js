bb417d279bae22db0b76d227d84a61a7ce1ef5d6
nikki-ha <nikkiha811@gmail.com>
2023-01-28 18:50:50 -0800

    Restructing and making a feasible skeleton because of time constraint.

--- frontend/src/screens/affiliates.js
+++ frontend/src/screens/affiliates.js
@@ -1,6 +1,5 @@
 import React from "react";
 import AffiliateComp from "../components/affiliateComp";
-import '../styles/affiliates.css';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 
---
+++ frontend/src/screens/info.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/info.css';
+
+const InfoScreen = () => {
+    return (
+        <div className="info-screen">
+            <h2 id="info-text">Info</h2>
+        </div>
+    )
+}
+
+export default InfoScreen;
