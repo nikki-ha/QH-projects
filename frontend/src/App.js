fb4bbb425e7891f3dd6c598f79241b475a859df6
May He <meiandpiano@gmail.com>
2023-01-28 19:21:49 -0800

    Navbar.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './styles/App.css'
 import HomeScreen from './screens/home.js';
+import NavbarComp from './components/NavComp';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <h1 id="title">QXR</h1>
-      <HomeScreen/>
+      <NavbarComp/>
     </div>
   );
 }
