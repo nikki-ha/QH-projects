fb82be0f9a6dbcd77bbf6735a4002b0d8204d7c7
nikki-ha <nikkiha811@gmail.com>
2023-01-28 17:40:49 -0800

    Home page updates.

--- frontend/src/screens/home.js
+++ frontend/src/screens/home.js
@@ -3,7 +3,10 @@ import '../styles/home.css';
 
 const HomeScreen = () => {
     return (
-        <h2>Welcome</h2>
+        <div className="home-screen">
+            <h2 id="welcome-text">Welcome</h2>
+            <p id="mission-statement">General Mission Statement.</p>
+        </div>
     )
 }
 
